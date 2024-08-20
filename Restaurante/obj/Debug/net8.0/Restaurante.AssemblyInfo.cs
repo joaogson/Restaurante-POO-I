0b43daeddf3a61f04fc8955a82259249872af00d
João Gabriel M. Sonalio <126979581+joaogson@users.noreply.github.com>
2024-08-20 14:00:32 -0300

    Restaurante com algumas atualizações
    metodos de gerenciamento de pratos e restaurantes implementados

--- Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
+++ Restaurante/obj/Debug/net8.0/Restaurante.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ec8d2378fef42a3471ee8c1957260deba592a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
